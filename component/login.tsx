1d8d7958757b61dd2b7693f7210cb5dc71d10196
Neel works <neelsoni.work@gmail.com>
2025-07-24 10:47:01 +0530

    github login added as well

--- component/login.tsx
+++ component/login.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Login() {
   const { data: session } = useSession();
@@ -9,15 +10,26 @@ export default function Login() {
     <main className="flex flex-col items-center justify-center h-screen">
       {session ? (
         <>
+          <Image
+            src={`${session.user?.image}`}
+            alt={""}
+            height={72}
+            width={72}
+          />
           <p className="mb-4">
             {session.user?.name}! you've already logged in.
           </p>
           <button onClick={() => signOut()}>Sign Out</button>
         </>
       ) : (
-        <button onClick={() => signIn("google", { callbackUrl: "/dashboard" })}>
-          Sign In
-        </button>
+        <div className="flex flex-col">
+          <button onClick={() => signIn("github")}>Sign In with GitHub</button>
+          <button
+            onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+          >
+            Sign In with Google
+          </button>
+        </div>
       )}
     </main>
   );
